1d846a6e8be490a8d0ea734967b5181bd0667920
Paul Haddad <paulh16@gmail.com>
2017-05-31 07:05:05 -0500

    Decode a Message Iteration * Continuously read in a message separated by commas and ending with EOL

--- pure_puzzles/decode_a_message.rb
+++ pure_puzzles/decode_a_message.rb
@@ -1,14 +1,34 @@
+require 'pry'
+
+def ascii_converter(character)
+  character.ord - "0".ord
+end
+
 def main
   # read in a stream of text until a line break is reached
 
   puts "Enter the coded message:"
 
   loop do
-    input_text = gets.chomp
-    break if input_text == ""
-  end
+    overall_int = 0
 
-  puts "end of line"
+    loop do
+      digit_character = gets.chomp
+
+      if digit_character == ","
+        puts overall_int
+        break
+      end
+
+      if digit_character == ""
+        puts overall_int
+        return
+      end
+
+      overall_int = (overall_int * 10 + ascii_converter(digit_character))
+    end
+  end
 end
 
 main
+
