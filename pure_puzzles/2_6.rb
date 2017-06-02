1bd606e0e74f6af3665e4ca72de50fbdb3615569
Paul Haddad <paulh16@gmail.com>
2017-06-02 07:16:04 -0500

    Problem 2-6

---
+++ pure_puzzles/2_6.rb
@@ -0,0 +1,40 @@
+require 'pry'
+
+class BinaryDecimalConverter
+  def self.to_binary(decimal_number)
+    binary_digits = []
+    sign = sign(decimal_number)
+    decimal_number_abs = decimal_number.abs
+
+    while decimal_number_abs > 0
+      binary_digits << decimal_number_abs % 2
+      decimal_number_abs /= 2
+    end
+
+    binary_number = ""
+    until binary_digits.empty?
+      binary_number += binary_digits.pop.to_s
+    end
+
+    sign * binary_number.to_i
+  end
+
+  def self.to_decimal(binary_number)
+    binary_digits = binary_number.to_s.split("").map(&:to_i)
+    max_digits_place = binary_digits.size - 1
+    sign = sign(binary_number)
+
+    decimal_number = 0
+    max_digits_place.downto(0) do |digit_place|
+      decimal_number += sign * binary_digits[max_digits_place - digit_place] * 2 ** digit_place
+    end
+
+    decimal_number
+  end
+
+  private
+
+  def self.sign(number)
+    number.zero? ? 1 : number / number.abs
+  end
+end
