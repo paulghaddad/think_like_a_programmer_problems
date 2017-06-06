66624c66e9cff7b55975dd44369c8443caed2acb
Paul Haddad <paulh16@gmail.com>
2017-06-06 07:05:05 -0500

    Add specs to prove Problem 2-8 works

--- pure_puzzles/number_system_converter_spec.rb
+++ pure_puzzles/number_system_converter_spec.rb
@@ -46,6 +46,24 @@ describe NumberSystemConverter do
 
       expect(binary_number).to eq("7E")
     end
+
+    it "converts from base 9 to base 4" do
+      base_9_number = 16
+      converter = NumberSystemConverter.new(number: base_9_number, base: 9)
+
+      base_4_number = converter.to_base(4)
+
+      expect(base_4_number).to eq("33")
+    end
+
+    it "converts from base 4 to base 9" do
+      base_4_number = 33
+      converter = NumberSystemConverter.new(number: base_4_number, base: 4)
+
+      base_9_number = converter.to_base(9)
+
+      expect(base_9_number).to eq("16")
+    end
   end
 
   context "negative digit" do
