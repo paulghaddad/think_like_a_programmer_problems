a511e8c0ff001b5958fd1c4a7c37c96d4dabefe0
Paul Haddad <paulh16@gmail.com>
2017-06-26 08:00:27 -0500

    Problem 5-2: Add description and age methods to Automobile class

--- classes/class_roster/automobile.rb
+++ classes/class_roster/automobile.rb
@@ -1,9 +1,19 @@
+require "date"
+
 class Automobile
   attr_reader :manufacturer, :model, :year
 
   def initialize(manufacturer:, model:, year:)
     @manufacturer = manufacturer
     @model = model
-    @year = year
+    @year = DateTime.new(year).year
+  end
+
+  def description
+    "%s %s %s" % [year, manufacturer, model]
+  end
+
+  def age
+    DateTime.now.year - year
   end
 end
--- classes/class_roster/automobile_spec.rb
+++ classes/class_roster/automobile_spec.rb
@@ -6,8 +6,28 @@ describe Automobile do
       new_car = Automobile.new(manufacturer: "Toyota", model: "Camry", year: 2016)
 
       expect(new_car).to have_attributes(manufacturer: "Toyota",
-                                        model: "Camry",
-                                        year: 2016)
+                                         model: "Camry",
+                                         year: 2016)
+    end
+  end
+
+  describe "description" do
+    it "returns a description of the vehicle" do
+      new_car = Automobile.new(manufacturer: "Toyota", model: "Camry", year: 2016)
+
+      description = new_car.description
+
+      expect(description).to match("2016 Toyota Camry")
+    end
+  end
+
+  describe "age" do
+    it "returns the age of the vehicle" do
+      new_car = Automobile.new(manufacturer: "Toyota", model: "Camry", year: 2016)
+
+      age = new_car.age
+
+      expect(age).to eq 1
     end
   end
 end
