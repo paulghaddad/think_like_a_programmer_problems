fd1b297a1a59aebd9389321efb1c8d5f8773a90e
Paul Haddad <paulh16@gmail.com>
2017-07-11 07:35:59 -0500

    Add a second solution for 6.2

--- recursion/6_2.rb
+++ recursion/6_2.rb
@@ -6,4 +6,20 @@ class BinaryStringAnalyzer
 
     first_char.odd? ^ self.bool?(binary_string[1..-1])
   end
+
+  def self.bool_2?(binary_string)
+    odd_count = self.odd_count(binary_string)
+    odd_count.odd?
+  end
+
+  def self.odd_count(binary_string)
+    return 0 if binary_string == ""
+    odd_count = odd_count(binary_string[0..-2])
+
+    if binary_string[-1].to_i.odd?
+      odd_count += 1
+    else
+      odd_count
+    end
+  end
 end
--- recursion/6_2_spec.rb
+++ recursion/6_2_spec.rb
@@ -18,4 +18,40 @@ describe "Problem 6.2" do
       end
     end
   end
+
+  describe "#boll_2?" do
+    context "binary string with odd parity" do
+      it "returns true" do
+        binary_string = "01000110"
+
+        expect(BinaryStringAnalyzer.bool_2?(binary_string)).to be true
+      end
+    end
+
+    context "binary string with even parity" do
+      it "returns false" do
+        binary_string = "01000010"
+
+        expect(BinaryStringAnalyzer.bool_2?(binary_string)).to be false
+      end
+    end
+  end
+
+  describe "#odd_count" do
+    context "binary string with odd parity" do
+      it "returns count of odd digits" do
+        binary_string = "01000110"
+
+        expect(BinaryStringAnalyzer.odd_count(binary_string)).to eq(3)
+      end
+    end
+
+    context "binary string with even parity" do
+      it "returns count of odd digits" do
+        binary_string = "01000010"
+
+        expect(BinaryStringAnalyzer.odd_count(binary_string)).to eq(2)
+      end
+    end
+  end
 end
