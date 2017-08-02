b8d375fd84190c05d4c4af39de08e73fb3569933
Paul Haddad <paulh16@gmail.com>
2017-08-02 06:43:50 -0500

    Problem 7.3: Implement find using the interpolation search algorithm

--- code_reuse/student_records/student_collection_with_array.rb
+++ code_reuse/student_records/student_collection_with_array.rb
@@ -12,6 +12,14 @@ class StudentCollectionWithArray
     @students << new_student
   end
 
+  def all_records
+    students.sort_by(&:id)
+  end
+
+  def student_count
+    students.count
+  end
+
   def average_grade
     num_students = 0
     sum = 0.0
@@ -24,5 +32,24 @@ class StudentCollectionWithArray
     sum / num_students
   end
 
-  alias_method :all_records, :students
+  # Using Interpolation Search Algorithm
+  def find(student_id)
+    low = 0
+    high = student_count - 1
+
+    while (low <= high && student_id >= all_records[low].id && student_id <= all_records[high].id)
+      position = low + ( (student_id - all_records[low].id) * (high - low) / (all_records[high].id - all_records[low].id))
+
+      if all_records[position].id == student_id
+        return position
+      elsif all_records[position].id < student_id
+        low = position + 1
+      else
+        high = position - 1
+      end
+    end
+
+    # student id not found
+    return -1
+  end
 end
--- code_reuse/student_records/student_collection_with_array_spec.rb
+++ code_reuse/student_records/student_collection_with_array_spec.rb
@@ -14,14 +14,28 @@ describe StudentCollectionWithArray do
   end
 
   describe "#all_records" do
-    it "returns all student records in the collection" do
-      student_1 = StudentRecord.new(grade: 90, id: 1575, name: "Steve")
-      student_2 = StudentRecord.new(grade: 95, id: 1576, name: "Bob")
-      student_collection = build_student_collection(student_1, student_2)
+    it "returns all student records in the collection ordered by student number" do
+      student_1 = StudentRecord.new(grade: 90, id: 3, name: "Steve")
+      student_2 = StudentRecord.new(grade: 95, id: 1, name: "Bob")
+      student_3 = StudentRecord.new(grade: 95, id: 2, name: "Bob")
+      student_collection = build_student_collection(student_1, student_2, student_3)
 
       student_records = student_collection.all_records
 
-      expect(student_records).to contain_exactly(student_1, student_2)
+      expect(student_records).to eq([student_2, student_3, student_1])
+    end
+  end
+
+  describe "#student_count" do
+    it "returns the number of students" do
+      student_1 = StudentRecord.new(grade: 94, id: 1575, name: "Chris")
+      student_2 = StudentRecord.new(grade: 100, id: 1576, name: "Bob")
+      student_3 = StudentRecord.new(grade: 90, id: 1577, name: "Adam")
+      student_collection = build_student_collection(student_1, student_2, student_3)
+
+      average_grade = student_collection.student_count
+
+      expect(average_grade).to eq(3)
     end
   end
 
@@ -35,6 +49,34 @@ describe StudentCollectionWithArray do
       average_grade = student_collection.average_grade
 
       expect(average_grade).to be_within(0.1).of(94.6)
+    end
+  end
+
+  describe "#search_by_student_id" do
+    context "student number present" do
+      it "returns the student's position in the student collection" do
+        student_1 = StudentRecord.new(grade: 94, id: 1575, name: "Chris")
+        student_2 = StudentRecord.new(grade: 100, id: 1574, name: "Bob")
+        student_3 = StudentRecord.new(grade: 90, id: 1577, name: "Adam")
+        student_collection = build_student_collection(student_1, student_2, student_3)
+
+        position = student_collection.find(1575)
+
+        expect(position).to eq(1)
+      end
+    end
+
+    context "student number not present" do
+      it "returns -1" do
+        student_1 = StudentRecord.new(grade: 94, id: 1575, name: "Chris")
+        student_2 = StudentRecord.new(grade: 100, id: 1574, name: "Bob")
+        student_3 = StudentRecord.new(grade: 90, id: 1577, name: "Adam")
+        student_collection = build_student_collection(student_1, student_2, student_3)
+
+        position = student_collection.find(1)
+
+        expect(position).to eq(-1)
+      end
     end
   end
 
