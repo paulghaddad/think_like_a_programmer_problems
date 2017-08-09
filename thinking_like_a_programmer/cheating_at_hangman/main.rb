087e71acaa1e5109b373f4631e18221df838e2da
Paul Haddad <paulh16@gmail.com>
2017-08-09 06:38:11 -0500

    Refactor and add display_word_list method

--- thinking_like_a_programmer/cheating_at_hangman/main.rb
+++ thinking_like_a_programmer/cheating_at_hangman/main.rb
@@ -1,13 +1,27 @@
 require 'pry'
 require 'set'
 
-word_list = Set.new
 
-File.open("word_list.txt") do |f|
-  f.each do |word|
-    unless word.match("\'")
-      word_list << word.gsub("\n", "")
+def read_word_file(path)
+  word_list = Set.new
+
+  File.open(path) do |f|
+    f.each do |word|
+      unless word.match("\'")
+        word_list << word.gsub("\n", "")
+      end
     end
+  end
+
+  word_list
+end
+
+def display_word_list(list)
+  binding.pry
+  list.each do |word|
+    puts word
   end
 end
 
+word_list = read_word_file("word_list.txt")
+display_word_list(word_list)
